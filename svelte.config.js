3b4b970c0c7995daedd792113061bfc419230bba
Luigi Morel <morelmiles@gmail.com>
2025-02-27 10:22:23 +0300

    fix: use netlify adpater

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,7 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: vitePreprocess(),
+export default {
 	kit: {
 		adapter: adapter()
 	}
 };
-
-export default config;

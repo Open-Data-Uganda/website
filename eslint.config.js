2a58e3db28aaf77a16205026f7ec3b73d24afba7
Luigi Morel <morelmiles@gmail.com>
2025-04-11 05:46:54 +0300

    fix: eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,38 @@
-import js from "@eslint/js";
-import tailwindcss from "eslint-plugin-tailwindcss";
+import js from '@eslint/js'
+import tailwindcss from 'eslint-plugin-tailwindcss'
+import globals from 'globals'
 
 export default [
   js.configs.recommended,
-  js.configs.node,
+
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
   {
     plugins: {
-      tailwindcss,
+      tailwindcss
     },
     rules: {
-      "tailwindcss/classnames-order": "warn",
+      'tailwindcss/classnames-order': 'warn'
     },
-  },
-];
+
+    ignores: [
+      'public/**/*',
+      '**/node_modules/**',
+      'resources/**',
+      'dist/**',
+      'assets/**',
+      'themes/opu/assets/**',
+      'themes/opu/node_modules/**',
+      '.husky/**',
+      '.cache/**'
+    ]
+  }
+]
